db24059854c944af15c822aff72662cdbbdfee44
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-29 16:52:01 -0800

    WIP
    Co-authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

--- src/index.js
+++ src/index.js
@@ -27,58 +27,58 @@ function searchResultAppend(label, thumbNail) {
 // display recipe on page
 // run displayRecipe() when desired recipe is selected from outcome list
 // should use jQuery to clear list before displaying? Or do in this function?
-function displayRecipe(result, recipeIndex) {
-  let rI = recipeIndex;
-  for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
-    let quantity = result.hits[rI].recipe.ingredients[i].quantity;
-    let measurement = result.hits[rI].recipe.ingredients[i].measurement;
-    let food = result.hits[rI].recipe.ingredients[i].food;
-    recipeListAppend(quantity, measurement, food);
-  }
-}
+// function displayRecipe(result, recipeIndex) {
+//   let rI = recipeIndex;
+//   for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
+//     let quantity = result.hits[rI].recipe.ingredients[i].quantity;
+//     let measurement = result.hits[rI].recipe.ingredients[i].measurement;
+//     let food = result.hits[rI].recipe.ingredients[i].food;
+//     recipeListAppend(quantity, measurement, food);
+//   }
+// }
 
 // run scaler on recipe
 // should use jQuery to clear list before displaying? Or do in this function?
-function recipeScaler(result, recipeIndex, scaleAmount) {
-  let rI = recipeIndex;
-  for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
-    let quantity = result.hits[rI].recipe.ingredients[i].quantity;
-    let measurement = result.hits[rI].recipe.ingredients[i].measurement;
-    let food = result.hits[rI].recipe.ingredients[i].food;
-    const scaledIngredient = scaler(quantity, measurement, scaleAmount);
-    measurement = scaledIngredient.join(" ");
-    recipeListAppend(quantity, measurement, food);
-  }
-}
+// function recipeScaler(result, recipeIndex, scaleAmount) {
+//   let rI = recipeIndex;
+//   for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
+//     let quantity = result.hits[rI].recipe.ingredients[i].quantity;
+//     let measurement = result.hits[rI].recipe.ingredients[i].measurement;
+//     let food = result.hits[rI].recipe.ingredients[i].food;
+//     const scaledIngredient = scaler(quantity, measurement, scaleAmount);
+//     measurement = scaledIngredient.join(" ");
+//     recipeListAppend(quantity, measurement, food);
+//   }
+// }
 
 
 
-function recipeListAppend(quantity, measurement, food) {
-  return `<li class="ingredientItem">
-  <span class="qty">${quantity}</span>
-  <span class="msrmt">${measurement}</span>
-  <span class="food">${food}</span>
-  </li>`;
-}
+// function recipeListAppend(quantity, measurement, food) {
+//   return `<li class="ingredientItem">
+//   <span class="qty">${quantity}</span>
+//   <span class="msrmt">${measurement}</span>
+//   <span class="food">${food}</span>
+//   </li>`;
+// }
 
-// what each should look like
-{/* <li class="ingredientItem">
-  <span class="qty"></span>
-  <span class="msrmt"></span>
-  <span class="food"></span>
-</li>  */}
+// // what each should look like
+// {/* <li class="ingredientItem">
+//   <span class="qty"></span>
+//   <span class="msrmt"></span>
+//   <span class="food"></span>
+// </li>  */}
 
 
 
-async function makeApiCall() {
-  const response = await RecipeAPI.getRecipe();
-  return response;
+async function makeApiCall(ingredient) {
+  const response = await RecipeAPI.getRecipe(ingredient);
+  displaySearchResult(response);
 }
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    clearFields();
-    makeApiCall(city);
+  $('#formSearchRecipes').submit(function(event) {
+    event.preventDefault();
+    let userSearch  = $('#search').val();
+    makeApiCall(userSearch);
   });
 });
--- src/js/recipe-API.js
+++ src/js/recipe-API.js
@@ -1,7 +1,7 @@
 export default class RecipeAPI {  
   static async getRecipe(ingredient) {
     try {
-      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=broccoli&app_id=3ba161e2&app_key=a4e4701171c306da3f5abfec16c0b11c`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
       if (!response.ok) {
         throw new Error(response.statusText);
       }
