f4467f8e8f588e5a498671c0515b0ff77b16fe50
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-30 10:15:13 -0800

    adisplay recipe list
    Co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,72 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { scaler } from '.js/scaler.js';
-import { RecipeAPI } from './js/recipe-API';
+// import { scaler } from '.js/scaler.js';
+import RecipeAPI from './js/recipe-API.js';
 
 function displaySearchResult(result) {
-  for (let i = 0; i < result.hits[i].recipe.length; i++) {
+  for (let i = 0; i < result.hits.length; i++) {
     let label = result.hits[i].recipe.label;
     let thumbNail = result.hits[i].recipe.images.THUMBNAIL.url;
-    searchResultAppend(label, thumbNail);
+    $("#recipeList").append(`<li class ="clickRecipe" id="${i}">
+    <span class="label">${label}</span>
+    <span class="thumbNail">${thumbNail}</span>
+    <div class="individualRecipe" id="recipe${i}"></div>
+    </li>`);
+    displayRecipe(result, i);
   }
 }
 
-function searchResultAppend(label, thumbNail) {
-  return `<li class ="recipeList">
-  <span class="label">${label}</span>
-  <span class="thumbNail">${thumbNail}</span>
-  </li>`
+function displayRecipe(result, recipeIndex) {
+  let rI = recipeIndex;
+  for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
+    let quantity = result.hits[rI].recipe.ingredients[i].quantity;
+    let measurement = result.hits[rI].recipe.ingredients[i].measure;
+    let food = result.hits[rI].recipe.ingredients[i].food;
+    $(`#recipe${rI}`).append(`<li class="ingredientItem">
+    <span class="qty">${quantity}</span>
+    <span class="msrmt">${measurement}</span>
+    <span class="food">${food}</span>
+    </li>`);
+  }
 }
-
-
-
-
-
-// display recipe on page
-// run displayRecipe() when desired recipe is selected from outcome list
-// should use jQuery to clear list before displaying? Or do in this function?
-// function displayRecipe(result, recipeIndex) {
-//   let rI = recipeIndex;
-//   for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
-//     let quantity = result.hits[rI].recipe.ingredients[i].quantity;
-//     let measurement = result.hits[rI].recipe.ingredients[i].measurement;
-//     let food = result.hits[rI].recipe.ingredients[i].food;
-//     recipeListAppend(quantity, measurement, food);
-//   }
-// }
-
-// run scaler on recipe
-// should use jQuery to clear list before displaying? Or do in this function?
-// function recipeScaler(result, recipeIndex, scaleAmount) {
-//   let rI = recipeIndex;
-//   for (let i = 0; i < result.hits[rI].recipe.ingredients.length; i++) {
-//     let quantity = result.hits[rI].recipe.ingredients[i].quantity;
-//     let measurement = result.hits[rI].recipe.ingredients[i].measurement;
-//     let food = result.hits[rI].recipe.ingredients[i].food;
-//     const scaledIngredient = scaler(quantity, measurement, scaleAmount);
-//     measurement = scaledIngredient.join(" ");
-//     recipeListAppend(quantity, measurement, food);
-//   }
-// }
-
-
-
-// function recipeListAppend(quantity, measurement, food) {
-//   return `<li class="ingredientItem">
-//   <span class="qty">${quantity}</span>
-//   <span class="msrmt">${measurement}</span>
-//   <span class="food">${food}</span>
-//   </li>`;
-// }
-
-// // what each should look like
-// {/* <li class="ingredientItem">
-//   <span class="qty"></span>
-//   <span class="msrmt"></span>
-//   <span class="food"></span>
-// </li>  */}
-
 
 
 async function makeApiCall(ingredient) {
@@ -75,10 +38,13 @@ async function makeApiCall(ingredient) {
   displaySearchResult(response);
 }
 
-$(document).ready(function() {
-  $('#formSearchRecipes').submit(function(event) {
-    event.preventDefault();
-    let userSearch  = $('#search').val();
-    makeApiCall(userSearch);
-  });
+
+$('#initialSearchSubmit').click(function(event) {
+  event.preventDefault();
+  let userSearch  = $('#search').val();
+  makeApiCall(userSearch);
+});
+
+$('.clickRecipe').click(function(event) {
+  $(".individualRecipe").toggle(500);
 });
--- src/js/recipe-API.js
+++ src/js/recipe-API.js
@@ -1,4 +1,4 @@
-export default class RecipeAPI {  
+export default class RecipeAPI {
   static async getRecipe(ingredient) {
     try {
       const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`);
