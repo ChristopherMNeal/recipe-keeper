b916c7f060fc6922530453be213b49c3decab8a5
Curtis Brooks <curtisbrooks678@gmail.com>
2021-12-01 16:18:30 -0800

    merging branches
    Co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>
    Co-authored by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>

--- src/js/scaler.js
+++ src/js/scaler.js
@@ -1,11 +1,6 @@
 function unitLogic(iterator, remainder, measurementArray, unitArray, smallestUnitArray) {
-<<<<<<< HEAD
   const unit = Math.floor(remainder / smallestUnitArray[iterator]) + " " + unitArray[iterator];
   remainder = (remainder % smallestUnitArray[iterator]).toFixed(2);
-=======
-  const unit = Math.floor(" " + remainder / smallestUnitArray[iterator]) + " " + unitArray[iterator];
-  remainder = remainder % smallestUnitArray[iterator];
->>>>>>> main
   measurementArray.push(unit);
   return remainder;
 }
@@ -17,13 +12,8 @@ function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
   for (let i = smallestUnitArray.length; i >= 0; i--) {
     if (remainder >= smallestUnitArray[i] && remainder >= smallestUnitArray[0]) {
       remainder = unitLogic(i, remainder, measurementArray, unitArray, smallestUnitArray);
-<<<<<<< HEAD
       for (let j = smallestUnitArray.length; j >= 0; j--) {
         if (remainder < smallestUnitArray[0]) {
-=======
-      for (let j = smallestUnitArray.length; j > 0; j--) {
-        if (remainder < 1 ) {
->>>>>>> main
           return measurementArray;
         } else if (remainder >= smallestUnitArray[j]) {
           remainder = unitLogic(j, remainder, measurementArray, unitArray, smallestUnitArray);
@@ -49,7 +39,6 @@ function smallestUnit(convertedInput, unitArray, smallestUnitArray) {
 }
 
 export default function scalerLogic(input, unit, scale) {
-<<<<<<< HEAD
   const volSmallestUnit = [1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
   const volUnitArray = ["ml", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "liter", "gallon"];
   const massSmallestUnit = [1, 28.35, 453.59, 1000, 907185, 1000000];
@@ -62,25 +51,10 @@ export default function scalerLogic(input, unit, scale) {
   const volUsUnitArray = ["quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "gallon", "ton"];
   const massUsSmallestUnit = [3.54, 7.09, 14.17, 28.35, 453.59, 907185];
   const massUsUnitArray = ["eighth ounce", "quarter ounce", "half ounce", "ounce", "pound", "ton"];
-=======
-  const volSmallestUnit = [0, 1, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 1000, 3785];
-  const volUnitArray = ["zero", "ml", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "liter", "gallon"];
-  const massSmallestUnit = [0, 1, 28.35, 453.59, 1000, 907185, 1000000];
-  const massUnitArray = ["zero", "gram", "ounce", "pound", "kilogram", "ton", "metric ton"];
-  // const volMetricSmallestUnit = [0, 1, 1000];
-  // const volMetricUnitArray = ["zero", "ml", "liter"];
-  // const massMetricSmallestUnit = [0, 1, 1000, 1000000];
-  // const massMetricUnitArray = ["zero", "gram", "kilogram", "metric ton"];
-  // const volUsSmallestUnit = [0, 1.23, 2.46, 4.93, 14.79, 59, 78, 118, 236.6, 473.2, 946.3, 3785];
-  // const volUsUnitArray = ["zero", "quarter tsp", "half tsp", "teaspoon", "tablespoon", "quarter cup", "third cup", "half cup", "cup", "pint", "quart", "gallon", "ton"];
-  // const massUsSmallestUnit = [0, 7.08 ,14.17, 21.26, 28.35, 453.59, 1000, 907185, 1000000];
-  // const massUsUnitArray = ["zero", "quarter ounce", "half ounce", "three-quarter ounce", "ounce", "pound", "kilogram", "ton", "metric ton"];
->>>>>>> main
   let convertInput = 0;
   let result = "";
   if (volUnitArray.indexOf(unit) != -1) {
     convertInput = input * volSmallestUnit[volUnitArray.indexOf(unit)] * scale;
-<<<<<<< HEAD
     if (convertInput < 1) {
       return `${input} ${unit}`;
     } else if (volMetricUnitArray.indexOf(unit) != -1){
@@ -111,15 +85,5 @@ export default function scalerLogic(input, unit, scale) {
     return "";
   } else {
     return (input * scale) + " " + unit;
-=======
-    return smallestUnit(convertInput, volUnitArray, volSmallestUnit).join(", ");
-  } else if (massUnitArray.indexOf(unit) != -1) {
-    convertInput = input * massSmallestUnit[massUnitArray.indexOf(unit)] * scale;
-    return smallestUnit(convertInput, massUnitArray, massSmallestUnit).join(", ");
-  } else if (unit === null) {
-    return "";
-  } else {
-    return (input * scale).toFixed(2) + " " + unit;
->>>>>>> main
   }
 }
